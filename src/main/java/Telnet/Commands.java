71d48ecf221f7282b29afc96a8daa0a09e6d2fdc
Thomas Øverby <thomas.overby@embriq.no>
2015-10-22 14:05:20 +0200

    Initial version of telnet server

---
+++ src/main/java/Telnet/Commands.java
@@ -0,0 +1,10 @@
+package Telnet;
+
+/**
+ * Created by Thomas on 22.10.2015.
+ */
+public class Commands {
+  protected static final String RELAY_LIST_TOURNAMENTS = "tell relay listtourney";
+  protected static final String RELAY_LIST_GAMES = "tell relay listgames";
+  protected static final String RELAY_LIST_GAMES_TOURN_NR= "tell relay listgames {}";
+}
---
+++ src/main/java/Telnet/TelnetServer.java
@@ -0,0 +1,96 @@
+package Telnet;
+
+import org.apache.commons.net.telnet.TelnetClient;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+public class TelnetServer {
+    public static final String USERNAME = "chessMatches";
+
+    public static final String PASSWORD = "tyuevd";
+
+    private TelnetClient telnet = new TelnetClient();
+
+    private InputStream in;
+
+    private PrintStream out;
+
+    private String prompt = "%";
+
+    public TelnetServer(String server, int port) {
+        connect(server, port);
+        login();
+    }
+
+    private void login() {
+        try {
+            readUntil("login: ");
+            write(USERNAME);
+            readUntil("password: ");
+            write(PASSWORD);
+            readUntil(prompt + " ");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void connect(String server, int port) {
+        try {
+            telnet.connect(server, port);
+            in = telnet.getInputStream();
+            out = new PrintStream(telnet.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readUntil(String pattern) {
+        try {
+            char lastChar = pattern.charAt(pattern.length() - 1);
+            StringBuffer sb = new StringBuffer();
+            char ch = (char) in.read();
+            while (true) {
+                System.out.print(ch);
+                sb.append(ch);
+                if (ch == lastChar) {
+                    if (sb.toString().endsWith(pattern)) {
+                        return sb.toString();
+                    }
+                }
+                ch = (char) in.read();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void write(String value) {
+        try {
+            out.println(value);
+            out.flush();
+            System.out.println(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String sendCommand(String command) {
+        try {
+            write(command);
+            return readUntil(prompt + " ");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void disconnect() {
+        try {
+            telnet.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
