c6777317bb37872481c596f2daa7c1f02aac05ac
Thomas <thomas.overby@embriq.no>
2015-11-03 22:12:27 +0100

    added firebase dao

--- src/main/java/persistence/dao/jpa/FirebaseDao.java
+++ src/main/java/persistence/dao/jpa/FirebaseDao.java
@@ -1,5 +1,15 @@
 package persistence.dao.jpa;
 
 
+import com.firebase.client.Firebase;
+
 public class FirebaseDao {
+    private static String firebaseURL = "https://chess-matches.firebaseio.com";
+    private static String email = "thomasboverby@gmail.com";
+    private static String password = "";
+    public void foo() {
+        Firebase firebase = new Firebase(firebaseURL);
+        firebase.authWithPassword(email, password, null );
+
+    }
 }
